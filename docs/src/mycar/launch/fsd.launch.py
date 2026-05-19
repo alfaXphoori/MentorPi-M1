a48a84daef7c1e3e5aba305626b3edd2b68dd89e
Phoori Chantima <alfa.phoori@gmail.com>
2026-05-19 14:37:01 +0700

    feat: include YOLOv5 launch in fsd.launch.py

--- docs/src/mycar/launch/fsd.launch.py
+++ docs/src/mycar/launch/fsd.launch.py
@@ -2,6 +2,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
     # Path to configuration file
@@ -10,9 +12,22 @@ def generate_launch_description():
         'config',
         'params.yaml'
     )
+    
+    # YOLOv5 Launch File Path
+    yolo_launch_path = os.path.join(
+        get_package_share_directory('yolov5_ros2'),
+        'launch',
+        'yolov5_ros2.launch.py'
+    )
 
     return LaunchDescription([
-        # 1. Camera Setup
+        # 1. Start YOLOv5 with specific model
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(yolo_launch_path),
+            launch_arguments={'model': 'traffic_signs_640s_7_0'}.items()
+        ),
+        
+        # 2. Camera Setup
         Node(
             package='mycar',
             executable='camera_tilt_node',
