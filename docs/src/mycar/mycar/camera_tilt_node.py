634bceebbc40cc21b3215d99e5e03225d8da7079
Phoori Chantima <alfa.phoori@gmail.com>
2026-05-19 13:19:57 +0700

    fix: update camera_tilt_node to use correct SetPWMServoState structure

--- docs/src/mycar/mycar/camera_tilt_node.py
+++ docs/src/mycar/mycar/camera_tilt_node.py
@@ -1,19 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from ros_robot_controller_msgs.msg import ServosPosition, ServoPosition
+from ros_robot_controller_msgs.msg import SetPWMServoState, PWMServoState
 import time
 
 class CameraTiltNode(Node):
     def __init__(self):
         super().__init__('camera_tilt_node')
-        # MentorPi M1 typically uses Bus Servos for camera tilt
-        self.publisher_ = self.create_publisher(ServosPosition, '/ros_robot_controller/bus_servo/set_position', 10)
+        # Correct topic for PWM servo state
+        self.publisher_ = self.create_publisher(SetPWMServoState, '/ros_robot_controller/pwm_servo/set_state', 10)
         
-        # Declare Parameters (Based on params.yaml)
+        # Declare Parameters
         self.declare_parameter('servo_id', 2)
-        self.declare_parameter('default_pos', 500) # Bus Servo Center
-        self.declare_parameter('min_pos', 300)     # Up
-        self.declare_parameter('max_pos', 700)     # Down
+        self.declare_parameter('default_pos', 1500) # PWM Center
+        self.declare_parameter('min_pos', 1200)     # Up
+        self.declare_parameter('max_pos', 1800)     # Down
 
         # Get Parameters
         self.servo_id = self.get_parameter('servo_id').get_parameter_value().integer_value
@@ -21,7 +21,7 @@ class CameraTiltNode(Node):
         self.min_pos = self.get_parameter('min_pos').get_parameter_value().integer_value
         self.max_pos = self.get_parameter('max_pos').get_parameter_value().integer_value
         
-        self.get_logger().info(f'Camera Tilt Node Started (Bus Servo ID: {self.servo_id}). Sweeping...')
+        self.get_logger().info(f'Camera Tilt Node Started (PWM ID: {self.servo_id}). Sweeping...')
         self.timer = self.create_timer(2.0, self.timer_callback)
         self.direction = 1
 
@@ -36,17 +36,17 @@ class CameraTiltNode(Node):
         self.set_tilt(self.current_pos)
 
     def set_tilt(self, position):
-        msg = ServosPosition()
-        msg.duration = 500 # Integer Milliseconds
+        msg = SetPWMServoState()
+        msg.duration = 0.5 # Float seconds
         
-        servo = ServoPosition()
-        servo.id = self.servo_id
-        servo.position = int(position)
+        servo = PWMServoState()
+        servo.id = [self.servo_id]
+        servo.position = [int(position)]
         
-        msg.position = [servo]
+        msg.state = [servo]
         
         self.publisher_.publish(msg)
-        self.get_logger().info(f'Setting Bus Servo Tilt to: {position}')
+        self.get_logger().info(f'Setting PWM Tilt to: {position}')
 
 def main(args=None):
     rclpy.init(args=args)
